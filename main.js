5e4fc639f68da8b354fdbd10f091a4a916045a80
1baisal3 <baisalbaizakov99088@gmail.com>
2023-08-17 18:52:20 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log('Hello word');
+
+// var num = 5
+// console.log(num);
+
+// var firstName, lastName;
+
+// firstName = 'Baisal'
+// lastName = ' Shailoobekov'
+// console.log(firstName, lastName);
+
+let test = 'test'
+let $ = 'test'
